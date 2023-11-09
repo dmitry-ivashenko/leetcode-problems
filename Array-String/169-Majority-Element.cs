16515fa48ef31bcd6009949bc441ec88c476225f
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-11-09 18:34:13 +0100

    Create 169-Majority-Element.cs

---
+++ Array-String/169-Majority-Element.cs
@@ -0,0 +1,42 @@
+// 169. Majority Element
+//
+// Given an array nums of size n, return the majority element.
+//
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+//
+// Example 1:
+//
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+//
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+//
+// Constraints:
+//
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+//
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        var counter = 1;
+        var element = nums[0];
+
+        for (var i = 0; i < nums.Length; i++){
+            var num = nums[i];
+            counter += element == num ? 1 : -1;
+            if (counter == 0){
+                counter = 1;
+                element = num;
+            }
+        }
+
+        return element;
+    }
+}
