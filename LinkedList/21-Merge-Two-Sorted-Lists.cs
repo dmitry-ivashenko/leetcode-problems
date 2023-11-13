354371db841ac5d25a836e160873b5d8db16d931
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-11-13 19:20:47 +0100

    Create 21-Merge-Two-Sorted-Lists.cs

---
+++ LinkedList/21-Merge-Two-Sorted-Lists.cs
@@ -0,0 +1,105 @@
+// 21. Merge Two Sorted Lists
+// 
+// You are given the heads of two sorted linked lists list1 and list2.
+// 
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// 
+// Return the head of the merged linked list.
+// 
+// Example 1:
+// 
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// 
+// 
+// Example 2:
+// 
+// Input: list1 = [], list2 = []
+// Output: []
+// 
+// 
+// Example 3:
+// 
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+//  
+// 
+// Constraints:
+// 
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+public class Solution 
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) 
+    {
+        if (list1 == null && list2 == null) 
+        {
+            return null;
+        }
+
+        if (list1 == null) 
+        {
+            return list2;
+        }
+
+        if (list2 == null) 
+        {
+            return list1;
+        }
+
+        ListNode head = null;
+        ListNode prev = null;
+
+        while (list1 != null || list2 != null)
+        {
+            ListNode current = null;
+
+            if (list1 == null) 
+            {
+                current = list2;
+                list2 = list2.next;
+            } 
+            else if (list2 == null) 
+            {
+                current = list1;
+                list1 = list1.next;
+            } 
+            else 
+            {
+                if (list1.val <= list2.val)
+                {
+                    current = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current = list2;
+                    list2 = list2.next;
+                }
+            }
+
+            if (prev != null) 
+                prev.next = current;
+            else
+                head = current;
+            
+            prev = current;
+        }
+
+        return head;
+    }
+}
