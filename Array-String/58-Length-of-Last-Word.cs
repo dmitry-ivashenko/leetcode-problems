0f13509e04feffcfddab5813df7179b4e0bc6dfc
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-11-19 16:31:52 +0100

    Create 58-Length-of-Last-Word.cs

---
+++ Array-String/58-Length-of-Last-Word.cs
@@ -0,0 +1,62 @@
+// 58. Length of Last Word
+//
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+//
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+//
+//
+//
+// Example 1:
+//
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+//
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+//
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+//
+//
+// Constraints:
+//
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        var wordDetected = false;
+        var lastPosition = s.Length - 1;
+        var startPosition = -1;
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            var c = s[i];
+            var isSpace = c == ' ';
+
+            if (wordDetected && isSpace)
+            {
+                startPosition = i;
+                break;
+            }
+
+            if (!wordDetected && !isSpace)
+            {
+                wordDetected = true;
+                lastPosition = i;
+            }
+        }
+
+        return lastPosition - startPosition;
+    }
+}
