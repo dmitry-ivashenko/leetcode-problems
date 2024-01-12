cc83df4499fe26f2f4df748ed9f99869138cc208
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2024-01-12 09:24:42 +0100

    Create 238-Product-of-Array-Except-Self.cs

---
+++ Array-String/238-Product-of-Array-Except-Self.cs
@@ -0,0 +1,54 @@
+// 238. Product of Array Except Self
+//
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements
+// of nums except nums[i].
+//
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+// Example 2:
+//
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+//
+//
+// Constraints:
+//
+// 2 <= nums.length <= 105
+// -30 <= nums[i] <= 30
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//
+//
+// Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space
+// for space complexity analysis.)
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var count = nums.Length;
+        var answer = new int[count];
+        var rightProduct = 1;
+
+        answer[0] = 1;
+        for (var i = 1; i < count; i++)
+        {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        for (var j = count - 1; j >= 0; j--)
+        {
+            answer[j] *= rightProduct;
+            rightProduct *= nums[j];
+        }
+
+        return answer;
+    }
+}
