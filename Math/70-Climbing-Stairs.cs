afe80e6ab74fd3fe1da2c0833fdda9bd0e4a32c2
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2024-01-20 13:18:12 +0100

    Create 70-Climbing-Stairs.cs

---
+++ Math/70-Climbing-Stairs.cs
@@ -0,0 +1,47 @@
+// 70. Climbing Stairs
+//
+// You are climbing a staircase. It takes n steps to reach the top.
+//
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//
+// Example 1:
+//
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+//
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+//
+//
+// Constraints:
+//
+// 1 <= n <= 45
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n <= 2)
+            return n;
+
+        var prev = 1;
+        var next = 2;
+
+        for (var i = 3; i <= n; i++)
+        {
+            var result = prev + next;
+            prev = next;
+            next = result;
+        }
+
+        return next;
+    }
+}
